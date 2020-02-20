9d4423b330c9c919b0203319146ae41d63eb6697
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-20 18:58:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str) {
+  const hour = parseInt(str.slice(0, 2));
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour >= 12 && hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(content) {
+  document.getElementById("greeting").innerText = content;
+}
